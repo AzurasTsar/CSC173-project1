32f6a74d2d6f86469d05cac2f6984fc215ccef7e
zendevil <pshar10@u.rochester.edu>
2017-09-14 19:10:31 -0400

    Add files via upload

---
+++ auto.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#define TRUE 1
+#define FALSE 0
+typedef int BOOLEAN;
+
+//find if a string is exactly 'ab'
+
+BOOLEAN findChar(char **a, char c) {
+
+  if(**a == c)
+  return TRUE;
+  else {
+    return FALSE;
+  }
+
+}
+
+BOOLEAN onlyAB(char *p) {
+  if(findChar(&p, 'a')) {
+    p++;
+    if(findChar(&p, 'b') ) {
+      p++;
+      if(findChar(&p, '\0'))
+        return TRUE;
+    }
+
+
+  }
+  return FALSE;
+
+}
+
+BOOLEAN startsAB(char *p) {
+  if(findChar(&p, 'a')) {
+    p++;
+    if(findChar(&p, 'b'))
+      return TRUE;
+  }
+  else {
+    return FALSE;
+  }
+}
+
+BOOLEAN even1s(char *p) {
+  int evenCounter = 0;
+
+  for(int i = 0; p[i] == '\0'; i++ ) {
+    if(p[i] == '1') {
+
+      evenCounter++;
+      printf("%d",i);
+
+    }
+
+    //i++;
+
+
+  }
+
+  printf("Even Counter Value: %d", evenCounter);
+  if(evenCounter % 2 == 0) {
+    return TRUE;
+  } else {
+    return FALSE;
+  }
+}
+
+int main() {
+
+  //automata only ab
+  /*
+  printf("%d\n", onlyAB("ab"));
+  printf("%d\n", onlyAB("tyab"));
+  printf("%d\n", onlyAB("abholyshit"));
+
+  //automata start with ab
+  printf("Starts ab: %d\n", startsAB("ab"));
+  printf("%d\n", startsAB("abstinence"));
+  printf("%d\n", startsAB("lkjhasldhk"));*/
+
+  //automata even 1s
+  printf("Even 1s test: %d\n", even1s("11010"));
+  printf("Even 1s test: %d\n", even1s("11110000"));
+}
